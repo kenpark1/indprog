7095132fd0394e96ad490a69f667b4a7b1b622ca
kenpark1 <76874976+kenpark1@users.noreply.github.com>
2023-05-29 00:04:44 +0300

    Add files via upload

---
+++ prac14/task22.cpp
@@ -0,0 +1,19 @@
+﻿﻿using namespace std;
+#include <iostream>
+#include <stdexcept>
+int main()
+{
+    int i;
+    int arr[5] = { 9,534,123,64,67 };
+    try {
+        cout << "vvedite index: ";
+        cin >> i;
+        if (i < 0 || i >= size(arr)) {
+            throw runtime_error("necorrectniy index\n");
+        }
+    }
+    catch (runtime_error& a) {
+        cout << "exception occured\n" << a.what() << "\n";
+    }
+    return 0;
+}
